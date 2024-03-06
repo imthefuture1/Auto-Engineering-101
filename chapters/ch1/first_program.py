584efd3dab3f5a5660fbe4b3e4574da693196b2a
thefuture1 <radjamohammad6@gmail.com>
2024-03-06 08:27:03 +0700

    Challenge1

---
+++ chapters/ch1/first_program.py
@@ -0,0 +1,7 @@
+print('Mohammad Radja')
+
+#tugas1
+Firstname=input('what your first name')
+Lastname=input('what your last name')
+Fullname= Firstname + Lastname
+print('Your full name is\n'+ Fullname)
